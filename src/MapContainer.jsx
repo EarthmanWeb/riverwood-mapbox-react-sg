6c8861f3af65cc8b0f0fb504f542ffcd52aa8a3b
Earthman Media <terran@earthman.ca>
2024-12-22 09:45:48 +0000

    Update start coordinates

--- src/MapContainer.jsx
+++ src/MapContainer.jsx
@@ -5,7 +5,7 @@ import { Map } from "./Map";
 export const MapContainer = ({ MapContext }) => {
   return (
     <MapContext.Provider value={{}}>
-      <Map lng={-81.97} lat={33.47} zoom={11} />
+      <Map lng={-84.388} lat={33.749} zoom={10} />
     </MapContext.Provider>
   );
 };
