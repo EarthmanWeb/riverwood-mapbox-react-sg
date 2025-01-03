1245188120b065efc164c9c466bd7cc59e1c5e5b
Earthman Media <terran@earthman.ca>
2025-01-03 10:09:15 +0000

    CSS tweaks, description safe html

--- src/components/PropertyDescription.jsx
+++ src/components/PropertyDescription.jsx
@@ -1,15 +1,37 @@
 import React from 'react';
+import DOMPurify from 'dompurify';
 import '../styles/components/PropertyDescription.css';
 import ErrorBoundary from './ErrorBoundary';
 
+const processHtmlLinks = (html) => {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(html, 'text/html');
+  
+  doc.querySelectorAll('a').forEach(link => {
+    link.setAttribute('target', '_blank');
+    link.setAttribute('rel', 'noopener noreferrer');
+  });
+  
+  return doc.body.innerHTML;
+};
+
 const PropertyDescriptionContent = ({ description }) => {
+  const processedHtml = processHtmlLinks(description);
+  const sanitizedHtml = DOMPurify.sanitize(processedHtml, {
+    ADD_ATTR: ['target']
+  });
+  
   return (
     <div>
       <h3>Description</h3>
-    <p>{description}</p>
+      <div 
+        className="property-description pb-4"
+        dangerouslySetInnerHTML={{ __html: sanitizedHtml }}
+      />
     </div>
   );
 };
+
 const PropertyDescription = (props) => (
   <ErrorBoundary>
     <PropertyDescriptionContent {...props} />
